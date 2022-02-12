b0a94070b81065ac17456b89db72d09fe6fa747c
Benito <xavier.benito@irta.cat>
2022-02-12 09:12:09 +0100

    update

--- R scripts/3-diatom-taxonomy-harmonisation.R
+++ R scripts/3-diatom-taxonomy-harmonisation.R
@@ -17,7 +17,9 @@ pacman::p_unload(pacman::p_loaded(), character.only = TRUE)
 library(tidyverse)
 library(stringr)
 library(qdapTools)
-library(algaeClassify) #something is wrong with this package
+library(algaeClassify) #something is wrong with this package as it doesn't realese anything
+library(worrms)
+library(xlsx)
 
 #####################
 ## Functions start ##
@@ -53,7 +55,7 @@ diatom_authority <- function(list_filename="",
   # algaebase <- spp_list_algaebase(conversion_df, phyto.name="CurrentDiatomSAname",long=FALSE,write=FALSE)
   # conversion_df$algaebase_synonyms <- algaebase$synonyms
   # conversion_df$algaebase_match <- algaebase$match.name
-  
+  # 
   #Create new column with spp authorities from Biodata
   #Replace var. for var to lookup
   diat_authorities_biodata <- BioData
@@ -63,7 +65,7 @@ diatom_authority <- function(list_filename="",
   
   # 
   # #Replace NAs in CurrentDiatomSAname column with original user name.
-  # conversion_df[is.na(conversion_df$authority_biodata)==TRUE,]$authority_biodata <- conversion_df[is.na(conversion_df$authority_biodata)==TRUE,]$UserTaxa
+  conversion_df[is.na(conversion_df$authority_biodata)==TRUE,]$authority_biodata <- conversion_df[is.na(conversion_df$authority_biodata)==TRUE,]$UserTaxa
   
   # 
   # # #Compare updated names (from Master list taxon) to algaebase and get the authority from Biodata
@@ -83,13 +85,35 @@ diatom_authority <- function(list_filename="",
   # conversion_df$authority_algaebase_omnidia <- lookup_e(conversion_df$algaebase_match, diat_authorities_omnidia[,c("DENOM3","DENOM")])
   # conversion_df[is.na(conversion_df$authority_algaebase_omnidia)==TRUE,]$authority_algaebase_omnidia <- conversion_df[is.na(conversion_df$authority_algaebase_omnidia)==TRUE,]$UserTaxa
   
+  #Get taxa information from the worms package
+  d <- seq_along(conversion_df$CurrentDiatomSAname) #create an index for each taxa name
+  d2 <- split(conversion_df$CurrentDiatomSAname, d) #make a list--one element for each spp
+  results <- list() #initialize the list where to store the results
+  #
+
+  # make a loop using try() to skip errors (i.e., those taxa not found)
+  for (i in seq_along(d2)) {
+    x <- d2[[i]]
+    results[i] <- try(wm_records_names(x, fuzzy=FALSE), silent = TRUE)
+  }
+
+  # extract the results from the list
+  worms <- do.call(rbind, results) 
+
+  # Create table with variables needed 
+  dtmBase <- select(worms, "AphiaID", "scientificname", "authority","status","citation", "rank", "genus", "family", "valid_AphiaID", "valid_name", "valid_authority")
+  
+  # Combine worms table with conversion_df table
+  conversion_df <- left_join(conversion_df, dtmBase, by = c("CurrentDiatomSAname" = "valid_name"))
+  
   #Convert conversion_df to dataframe.
   conversion_df <- data.frame(lapply(conversion_df, as.character), stringsAsFactors=FALSE)
   
-  #Replace diatom taxa updated names and write csv
-  #colnames(diat) <- conversion_df[,2]
-  
+  # Create an Excel file
+  write.xlsx(conversion_df, "data/diatom_master_taxon_list_authorities.xlsx", row.names = TRUE)
+
   return(conversion_df)
+  
 }
 
 # This function removes all text after the specific epithet or variety or form, etc.
@@ -132,7 +156,7 @@ Omnidia2015_database$DENOM3 <- sapply(Omnidia2015_database$DENOM2, truncAuthor) 
 
 #Read in dataset(s) to be harmonized
 diat <- read.csv("data/galapagos.csv", row.names = 1, sep = ";")
-#combined <- read.csv("data/assembledspp_new.csv", row.names=1)
+#diat <- read.csv("data/assembledspp_new.csv", row.names=1)
 
 #transform dataframe to tidy format
 df_thin <- diat %>%
@@ -145,9 +169,9 @@ changes_nms <- read.csv("data/old_new_nms_master_revised.csv", sep=";", stringsA
 
 #spread & fix some taxonomic errors with updated revised harmonized taxonomy
 diat <- df_thin %>%
+  mutate(taxa = plyr::mapvalues(taxa, from = changes_nms[,1], to = changes_nms$revised_harmonized_taxon_name))%>%
   mutate(taxa=str_replace(taxa, "Bacteriastrumÿhyalinum", "Bacteriastrum.hyalinum"))%>%
   mutate(taxa=str_replace(taxa, "Cyclostephanos.sp.1.ENCAÃ.ADO", "Cyclostephanos.sp.1.ENCANADO"))%>%
-  mutate(taxa = plyr::mapvalues(taxa, from = changes_nms[,2], to = changes_nms$revised_harmonized_taxon_name))%>%
   #mutate(taxa = plyr::mapvalues(taxa, from = changes_nms[,2], to = changes_nms$new_1))%>%
   mutate(taxa=factor(taxa))
 
@@ -155,14 +179,14 @@ diat <- df_thin %>%
 taxa_names <- data.frame(levels(diat$taxa))
 
 #replace points by space for running diatomAuthority function
-taxa_names <- data.frame(gsub("f.", "fo.", taxa_names[,1], fixed=TRUE), stringsAsFactors = FALSE) 
+#taxa_names <- data.frame(gsub("f.", "fo.", taxa_names[,1], fixed=TRUE), stringsAsFactors = FALSE) 
 taxa_names <- data.frame(gsub(".", " ", taxa_names[,1], fixed=TRUE), stringsAsFactors = FALSE) 
 taxa_names <- data.frame(gsub("  ", " ", taxa_names[,1], fixed=TRUE), stringsAsFactors = FALSE) 
 
 colnames(taxa_names) <- c("diat_name")
 #taxa_names <- taxa_names %>% top_n(40) #test the first 10
 
-# Apply diatom_harm()
+# Apply diatom_authority()
 list <- diatom_authority(list_filename="user_list", 
                          diatnames_filename="taxa_names", 
                          authority_list="Biodata",
@@ -170,13 +194,6 @@ list <- diatom_authority(list_filename="user_list",
 
 
 #Replace diatom taxa updated names and write csv
-colnames(diat) <- list[,2]
+#colnames(diat) <- list[,2]
 #write.xlsx(list, "data/diatom_master_taxon_list_authorities.xlsx", row.names = TRUE)
-
-
-## test for algaeClassify
-taxa_names <- data.frame(taxa_names[1:10,])
-names(taxa_names) <- "diat_name"
-
-algaebase <- spp_list_algaebase(conversion_df, phyto.name="diat_name",long=FALSE,write=FALSE)
 
